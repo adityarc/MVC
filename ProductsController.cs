7fda644d76e424fac497ea259066adefa441dd4f
Aditya Roy Choudhary <arclive202@gmail.com>
2018-02-26 15:54:09 +0530

    Add files via upload

---
+++ ProductsController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using tryingmvc.Models;
+
+namespace tryingmvc.Controllers
+{
+    
+    public class ProductsController : Controller
+    {
+      public ActionResult Index()
+        {
+            var Product = new Products
+            {
+                ProductID = 101,
+                ProductName = "Cell Phone",
+                Price = 25000,
+                Quantity = 50
+
+            };            return View(Product);
+        }
+
+    }
+}
