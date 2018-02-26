510cf11a9cb65e3d5471fb3de4260a48ddeeeede
Aditya Roy Choudhary <arclive202@gmail.com>
2018-02-26 17:41:40 +0530

    Add files via upload

---
+++ Customer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace tryingmvc.Models
+{
+    public class Customer
+    {
+        public int CustomerID { get; set; }
+        public string CustomerName { get; set; }
+        public string City { get; set; }
+        public string Phone { get; set; }
+    }
+
+}
---
+++ CustomerController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using tryingmvc.Models;
+
+namespace tryingmvc.Controllers
+{
+    public class CustomerController : Controller
+    {
+        // GET: Customer
+        public ActionResult CustomerIndex()
+        {
+            var Cust = GetCustomers();
+              
+             return View(Cust);
+        }
+
+        public List<Customer> GetCustomers()
+        {
+            return new List<Customer>
+            {
+                new Customer {CustomerID = 1, CustomerName = "Abhishek", City = "kolkata", Phone = "9874237432" },
+                new Customer {CustomerID = 2, CustomerName = "Raja", City = "Chennai", Phone = "9823437432" },
+                new Customer {CustomerID = 3, CustomerName = "Kiran", City = "Chennai", Phone = "9875677432" },
+                new Customer {CustomerID = 4, CustomerName = "Archi", City = "kolkata", Phone = "9872564432" },
+
+            };
+        }
+    }
+}
