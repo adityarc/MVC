e4b2522dfccb02da5badf52ff2d6da1cd177208b
Aditya Roy Choudhary <arclive202@gmail.com>
2018-02-27 18:01:36 +0530

    Add files via upload

---
+++ app.module.ts
@@ -0,0 +1,23 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+import { AppComponent } from './app.component';
+import { ProductComponent } from './product/product.component';
+import { ProductFormComponent } from './product-form/product-form.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ProductComponent,
+    ProductFormComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
